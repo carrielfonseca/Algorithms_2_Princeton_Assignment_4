ab8fccf4c185208bfba706e92d0bdebb64cbaf86
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-02-25 19:26:45 -0500

    method to get the vertexIndex of a matrix

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.TST;
 
 public class BoggleSolver {
 
-	TST<Boolean> dictionaryInTrie = new TST<>();
+	TST<Boolean> dictionaryInTrie = new TST<>(); // ternary search tries
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
@@ -18,6 +18,13 @@ public class BoggleSolver {
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
 //    public int scoreOf(String word)
+    
+    // returns the index of the cel in a matrix with a numberOfRows and a numberOfCols
+    // read from left to right, top to bottom
+    private int vertexIndex(int i, int j, int numberOfRows, int numberOfCols) {
+    	int vertexIndex =  i*numberOfCols + j;
+    	return vertexIndex;
+    }
 	
 	public static void main(String[] args) {
 	    
