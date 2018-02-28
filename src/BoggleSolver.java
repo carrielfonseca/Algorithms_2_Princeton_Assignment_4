1d769dbbb585e7883ec15a632e558be9da8284e9
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-02-27 20:47:27 -0500

    removes graph and introduces array of bags

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
@@ -10,7 +11,7 @@ import edu.princeton.cs.algs4.TST;
 public class BoggleSolver {
 
 	private TST<Boolean> dictionaryInTrie = new TST<>(); // ternary search tries
-	private Graph boggleGraph;
+	private Bag<Integer>[] adj; // adjacent squares that can be visited from each vertex
 	private boolean[] marked;  //if true, means the cell is already visited in a certain path in the Boggle Boad
 
     // Initializes the data structure using the given array of strings as the dictionary.
@@ -23,12 +24,15 @@ public class BoggleSolver {
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-    	boggleGraph = new Graph(board.rows()*board.cols());
+    	adj = (Bag<Integer>[]) new Bag[board.rows()*board.cols()];
+    	for (int v = 0; v < adj.length; v++) {
+            adj[v] = new Bag<Integer>();
+        }
     	marked = new boolean[board.rows()*board.cols()];
-    	buildsBoggleGraph(board, boggleGraph);
+    	buildsNeighboors(board);
     	Set<String> validWords = new HashSet<>();
     	for (int i = 0; i < board.rows()*board.cols(); i++) {    	
-    		visitSquare(board, boggleGraph, i ,"", validWords);    	
+    		visitSquare(board, i ,"", validWords);    	
     	}
     	return validWords;    	
     }
@@ -75,28 +79,29 @@ public class BoggleSolver {
     	return colOfVertex;
     }
     
-    private void addEdgesToNeighboors(Graph graph, int i, int j, int numberOfRows, int numberOfCols) {
+    private void addNeighboors(int i, int j, int numberOfRows, int numberOfCols) {
+    	int vertexIndex = vertexIndex(i, j, numberOfRows, numberOfCols);
     	for (int l = (i-1); l <= (i+1); l++) {
     		for (int m = (j-1); m <= (j+1); m++) {
     			//if within boudaries AND not the same square (cannot have self reference) 
     			
     			if (l >= 0 && l <= (numberOfRows-1) && m >= 0 && m <= (numberOfCols-1)
     			    && !(l == i && m == j)) {
-    				graph.addEdge(vertexIndex(i, j, numberOfRows, numberOfCols), vertexIndex(l, m, numberOfRows, numberOfCols));
+    				adj[vertexIndex].add(vertexIndex(l, m, numberOfRows, numberOfCols));
     			}
     		}
     	}
     }
     
-    private void buildsBoggleGraph(BoggleBoard board,Graph boggleGraph) {
+    private void buildsNeighboors(BoggleBoard board) {
 		for (int i = 0; i < board.rows(); i++) {
 			for (int j = 0; j < board.cols(); j++) {
-				addEdgesToNeighboors(boggleGraph, i, j, board.rows(), board.cols());
+				addNeighboors(i, j, board.rows(), board.cols());
 			}
 		}
     }
     
-    private void visitSquare(BoggleBoard board, Graph boggleGraph, int vertex, String word, Set<String> words) {
+    private void visitSquare(BoggleBoard board,  int vertex, String word, Set<String> words) {
     	marked[vertex] = true;
     	String boardLetter = "" +  board.getLetter(rowOfVertex(vertex,  board.rows(), board.cols()), colOfVertex(vertex,  board.rows(), board.cols()));
     	// makes a correction for the special case of letter "Q"
@@ -110,11 +115,11 @@ public class BoggleSolver {
     	if (dictionaryInTrie.contains(word) && lengthAdjusted >= 3) { 
     		words.add(word);
     	}
-    	for (int v : boggleGraph.adj(vertex)) {
+    	for (int v : adj[vertex]) {
     		Queue<String> keysWithPrefix = (Queue<String>) dictionaryInTrie.keysWithPrefix(word);
     		//does not need to visit a square if you know there arent any words that start with those caracters
     		if (!marked[v] && !keysWithPrefix.isEmpty()) {
-    			visitSquare(board, boggleGraph, v, word, words);
+    			visitSquare(board, v, word, words);
     		}
     	}
     	if (boardLetter.equalsIgnoreCase("Qu")) {
