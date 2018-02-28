905cb5356562f651d8254f685bca28b75562b20d
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-02-28 11:57:12 -0500

    class should be Multiwaytrie symbol

--- src/MultiwayTrieFabio.java
+++ src/MultiwayTrieFabio.java
@@ -1,64 +1,52 @@
-
-
-
-import java.util.Iterator;
-
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.TrieSET;
+import edu.princeton.cs.algs4.TrieST;
 
-/**
- *  The {@code TrieSET} class represents an ordered set of strings over
- *  the extended ASCII alphabet.
- *  It supports the usual <em>add</em>, <em>contains</em>, and <em>delete</em>
- *  methods. It also provides character-based methods for finding the string
- *  in the set that is the <em>longest prefix</em> of a given prefix,
- *  finding all strings in the set that <em>start with</em> a given prefix,
- *  and finding all strings in the set that <em>match</em> a given pattern.
- *  <p>
- *  This implementation uses a 256-way trie.
- *  The <em>add</em>, <em>contains</em>, <em>delete</em>, and
- *  <em>longest prefix</em> methods take time proportional to the length
- *  of the key (in the worst case). Construction takes constant time.
- *  <p>
- *  For additional documentation, see
- *  <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
- *  <i>Algorithms in Java, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
-public class MultiwayTrieFabio implements Iterable<String> {
+public class MultiwayTrieFabio<Value> {
     private static final int R = 256;        // extended ASCII
+
 
     private Node root;      // root of trie
     private int n;          // number of keys in trie
 
     // R-way trie node
     private static class Node {
+        private Object val;
         private Node[] next = new Node[R];
-        private boolean isString;
     }
 
-    /**
-     * Initializes an empty set of strings.
+   /**
+     * Initializes an empty string symbol table.
      */
     public MultiwayTrieFabio() {
     }
 
+
     /**
-     * Does the set contain the given key?
+     * Returns the value associated with the given key.
      * @param key the key
-     * @return {@code true} if the set contains {@code key} and
+     * @return the value associated with the given key if the key is in the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(String key) {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null");
+        Node x = get(root, key, 0);
+        if (x == null) return null;
+        return (Value) x.val;
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
      *     {@code false} otherwise
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
     public boolean contains(String key) {
         if (key == null) throw new IllegalArgumentException("argument to contains() is null");
-        Node x = get(root, key, 0);
-        if (x == null) return false;
-        return x.isString;
+        return get(key) != null;
     }
 
     private Node get(Node x, String key, int d) {
@@ -69,52 +57,55 @@ public class MultiwayTrieFabio implements Iterable<String> {
     }
 
     /**
-     * Adds the key to the set if it is not already present.
-     * @param key the key to add
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     * @param key the key
+     * @param val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void add(String key) {
-        if (key == null) throw new IllegalArgumentException("argument to add() is null");
-        root = add(root, key, 0);
+    public void put(String key, Value val) {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
+        if (val == null) delete(key);
+        else root = put(root, key, val, 0);
     }
 
-    private Node add(Node x, String key, int d) {
+    private Node put(Node x, String key, Value val, int d) {
         if (x == null) x = new Node();
         if (d == key.length()) {
-            if (!x.isString) n++;
-            x.isString = true;
+            if (x.val == null) n++;
+            x.val = val;
+            return x;
         }
-        else {
-            char c = key.charAt(d);
-            x.next[c] = add(x.next[c], key, d+1);
-        }
+        char c = key.charAt(d);
+        x.next[c] = put(x.next[c], key, val, d+1);
         return x;
     }
 
     /**
-     * Returns the number of strings in the set.
-     * @return the number of strings in the set
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
      */
     public int size() {
         return n;
     }
 
     /**
-     * Is the set empty?
-     * @return {@code true} if the set is empty, and {@code false} otherwise
+     * Is this symbol table empty?
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
      */
     public boolean isEmpty() {
         return size() == 0;
     }
 
     /**
-     * Returns all of the keys in the set, as an iterator.
-     * To iterate over all of the keys in a set named {@code set}, use the
-     * foreach notation: {@code for (Key key : set)}.
-     * @return an iterator to all of the keys in the set
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     * @return all keys in the symbol table as an {@code Iterable}
      */
-    public Iterator<String> iterator() {
-        return keysWithPrefix("").iterator();
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
     }
 
     /**
@@ -129,35 +120,54 @@ public class MultiwayTrieFabio implements Iterable<String> {
         collect(x, new StringBuilder(prefix), results);
         return results;
     }
+    
+	public boolean hasKeysWithPrefix(String prefix) {
+		Queue<String> queue = new Queue<String>();
+		Node x = get(root, prefix, 0);
+		collectKey(x, new StringBuilder(prefix), queue);
+		return !queue.isEmpty();
+	}
+
+
+	private void collectKey(Node x, StringBuilder prefix, Queue<String> queue) {
+		if (x == null)	return;
+		if (x.val != null)	queue.enqueue(prefix.toString());
+		for (char c = 0; c < R; c++) {
+			prefix.append(c);
+			collect(x.next[c], prefix, queue);
+			prefix.deleteCharAt(prefix.length() - 1);
+		}
+	}
 
     private void collect(Node x, StringBuilder prefix, Queue<String> results) {
         if (x == null) return;
-        if (x.isString) results.enqueue(prefix.toString());
+        if (x.val != null) results.enqueue(prefix.toString());
         for (char c = 0; c < R; c++) {
             prefix.append(c);
             collect(x.next[c], prefix, results);
             prefix.deleteCharAt(prefix.length() - 1);
         }
     }
+    
+    
 
     /**
-     * Returns all of the keys in the set that match {@code pattern},
+     * Returns all of the keys in the symbol table that match {@code pattern},
      * where . symbol is treated as a wildcard character.
      * @param pattern the pattern
-     * @return all of the keys in the set that match {@code pattern},
+     * @return all of the keys in the symbol table that match {@code pattern},
      *     as an iterable, where . is treated as a wildcard character.
-     */  
+     */
     public Iterable<String> keysThatMatch(String pattern) {
         Queue<String> results = new Queue<String>();
-        StringBuilder prefix = new StringBuilder();
-        collect(root, prefix, pattern, results);
+        collect(root, new StringBuilder(), pattern, results);
         return results;
     }
-        
+
     private void collect(Node x, StringBuilder prefix, String pattern, Queue<String> results) {
         if (x == null) return;
         int d = prefix.length();
-        if (d == pattern.length() && x.isString)
+        if (d == pattern.length() && x.val != null)
             results.enqueue(prefix.toString());
         if (d == pattern.length())
             return;
@@ -177,10 +187,10 @@ public class MultiwayTrieFabio implements Iterable<String> {
     }
 
     /**
-     * Returns the string in the set that is the longest prefix of {@code query},
+     * Returns the string in the symbol table that is the longest prefix of {@code query},
      * or {@code null}, if no such string.
      * @param query the query string
-     * @return the string in the set that is the longest prefix of {@code query},
+     * @return the string in the symbol table that is the longest prefix of {@code query},
      *     or {@code null} if no such string
      * @throws IllegalArgumentException if {@code query} is {@code null}
      */
@@ -188,16 +198,16 @@ public class MultiwayTrieFabio implements Iterable<String> {
         if (query == null) throw new IllegalArgumentException("argument to longestPrefixOf() is null");
         int length = longestPrefixOf(root, query, 0, -1);
         if (length == -1) return null;
-        return query.substring(0, length);
+        else return query.substring(0, length);
     }
 
     // returns the length of the longest string key in the subtrie
     // rooted at x that is a prefix of the query string,
     // assuming the first d character match and we have already
-    // found a prefix match of length length
+    // found a prefix match of given length (-1 if no such match)
     private int longestPrefixOf(Node x, String query, int d, int length) {
         if (x == null) return length;
-        if (x.isString) length = d;
+        if (x.val != null) length = d;
         if (d == query.length()) return length;
         char c = query.charAt(d);
         return longestPrefixOf(x.next[c], query, d+1, length);
@@ -216,8 +226,8 @@ public class MultiwayTrieFabio implements Iterable<String> {
     private Node delete(Node x, String key, int d) {
         if (x == null) return null;
         if (d == key.length()) {
-            if (x.isString) n--;
-            x.isString = false;
+            if (x.val != null) n--;
+            x.val = null;
         }
         else {
             char c = key.charAt(d);
@@ -225,55 +235,56 @@ public class MultiwayTrieFabio implements Iterable<String> {
         }
 
         // remove subtrie rooted at x if it is completely empty
-        if (x.isString) return x;
+        if (x.val != null) return x;
         for (int c = 0; c < R; c++)
             if (x.next[c] != null)
                 return x;
         return null;
     }
 
-
     /**
-     * Unit tests the {@code TrieSET} data type.
+     * Unit tests the {@code TrieST} data type.
      *
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        TrieSET set = new TrieSET();
-        while (!StdIn.isEmpty()) {
+
+        // build symbol table from standard input
+        TrieST<Integer> st = new TrieST<Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
             String key = StdIn.readString();
-            set.add(key);
+            st.put(key, i);
         }
 
         // print results
-        if (set.size() < 100) {
+        if (st.size() < 100) {
             StdOut.println("keys(\"\"):");
-            for (String key : set) {
-                StdOut.println(key);
+            for (String key : st.keys()) {
+                StdOut.println(key + " " + st.get(key));
             }
             StdOut.println();
         }
 
         StdOut.println("longestPrefixOf(\"shellsort\"):");
-        StdOut.println(set.longestPrefixOf("shellsort"));
+        StdOut.println(st.longestPrefixOf("shellsort"));
         StdOut.println();
 
-        StdOut.println("longestPrefixOf(\"xshellsort\"):");
-        StdOut.println(set.longestPrefixOf("xshellsort"));
+        StdOut.println("longestPrefixOf(\"quicksort\"):");
+        StdOut.println(st.longestPrefixOf("quicksort"));
         StdOut.println();
 
         StdOut.println("keysWithPrefix(\"shor\"):");
-        for (String s : set.keysWithPrefix("shor"))
-            StdOut.println(s);
-        StdOut.println();
-
-        StdOut.println("keysWithPrefix(\"shortening\"):");
-        for (String s : set.keysWithPrefix("shortening"))
+        for (String s : st.keysWithPrefix("shor"))
             StdOut.println(s);
         StdOut.println();
 
         StdOut.println("keysThatMatch(\".he.l.\"):");
-        for (String s : set.keysThatMatch(".he.l."))
+        for (String s : st.keysThatMatch(".he.l."))
             StdOut.println(s);
     }
 }
+
+    
+
+
+   
