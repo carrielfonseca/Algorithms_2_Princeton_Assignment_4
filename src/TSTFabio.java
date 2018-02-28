0b25fcf4426ff28a2db13eaa4d1c77a448aaad92
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-02-27 23:16:18 -0500

    removes java doc

--- src/TSTFabio.java
+++ src/TSTFabio.java
@@ -1,7 +1,6 @@
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.TST;
 
 /******************************************************************************
  *  Compilation:  javac TST.java
@@ -42,28 +41,7 @@ import edu.princeton.cs.algs4.TST;
  ******************************************************************************/
 
 
-/**
- *  The {@code TST} class represents an symbol table of key-value
- *  pairs, with string keys and generic values.
- *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
- *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
- *  It also provides character-based methods for finding the string
- *  in the symbol table that is the <em>longest prefix</em> of a given prefix,
- *  finding all strings in the symbol table that <em>start with</em> a given prefix,
- *  and finding all strings in the symbol table that <em>match</em> a given pattern.
- *  A symbol table implements the <em>associative array</em> abstraction:
- *  when associating a value with a key that is already in the symbol table,
- *  the convention is to replace the old value with the new value.
- *  Unlike {@link java.util.Map}, this class uses the convention that
- *  values cannot be {@code null}—setting the
- *  value associated with a key to {@code null} is equivalent to deleting the key
- *  from the symbol table.
- *  <p>
- *  This implementation uses a ternary search trie.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- */
+
 public class TSTFabio<Value> {
     private int n;              // size
     private Node<Value> root;   // root of TST
